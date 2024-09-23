613c3ea8e5ef7d4e0d3528d76ad76987d5acdabe
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-09-23 15:12:12 +0100

    Refactor database configuration and exports in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,52 +1,46 @@
 // Load node modules
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 
 const createDb = () => {
-    try{
-        //create a new database
-        let db = new sqlite3.Database('./poller.sqlite', sqlite3.OPEN_CREATE, (err) => {
-            if (err) {
-                console.error(err.message);
-            }
-            console.log('Connected to the database.');
-        });
-        db.serialize(() => {
-            //create two tables on for users and one for polls each poll has a user_id as a foreign key
-            db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)');
-            db.run("CREATE TABLE polls (id INTEGER PRIMARY KEY, question TEXT NOT NULL, yes INTEGER NULL, no INEGER NULL , created_on TEXT NOT NULL DEFAULT CURRENT_DATE, user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id))");
-            //create user who will represent all the polls that are not associated with a user
-            db.run('INSERT INTO users (username, password) VALUES ("guest", "guest")');
-        });
-        //close the database connection
-        db.close((err) => {
-            if (err) {
-                console.error(err.message);
-            }
-            console.log('Closed the database connection.');
-        });
-    } catch (error) {
-        console.error(error.message);
-    };
+    //create a new database
+    let db = new sqlite3.Database("./poller.sqlite", sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Connected to the database.");
+    });
+    db.serialize(() => {
+        //create two tables on for users and one for polls each poll has a user_id as a foreign key
+        db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)");
+        db.run("CREATE TABLE IF NOT EXISTS polls (id INTEGER PRIMARY KEY, question TEXT NOT NULL, yes INTEGER NULL, no INTEGER NULL , created_on TEXT NOT NULL DEFAULT CURRENT_DATE, user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id))");
+    });
+    //close the database connection
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Closed the database connection.");
+    });
 };
 
 const connectEditDb = () => {
     //open the database in readwrite mode
-    let db = new sqlite3.Database('./poller.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+    let db = new sqlite3.Database("./poller.sqlite", sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
             console.error(err.message);
         }
-        console.log('Connected to the database.');
+        console.log("Connected to the database.");
     });
     return db;
 };
 
 const connectReadDb = () => {
     //open the database in readonly mode
-    let db = new sqlite3.Database('./poller.sqlite', sqlite3.OPEN_READONLY, (err) => {
+    let db = new sqlite3.Database("./poller.sqlite", sqlite3.OPEN_READONLY, (err) => {
         if (err) {
             console.error(err.message);
         }
-        console.log('Connected to the database.');
+        console.log("Connected to the database.");
     });
     return db;
 };
@@ -57,9 +51,9 @@ const closeDb = (db) => {
         if (err) {
             console.error(err.message);
         }
-        console.log('Closed the database connection.');
+        console.log("Closed the database connection.");
     });
 };
 
 
-export default { createDb, connectEditDb, connectReadDb, closeDb };
+export { createDb, connectEditDb, connectReadDb, closeDb };
