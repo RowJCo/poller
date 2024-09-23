e9e4c42d8af5b93d857f0ec3a6ee72206f80d0b7
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-09-23 15:11:37 +0100

    Refactor deleteGuestPolls worker to delete polls associated with guest users created more than 48 hours ago

--- backend/workers/deleteGuestPolls.js
+++ backend/workers/deleteGuestPolls.js
@@ -1,20 +1,19 @@
 // Load custom modules
-import { connectEditDb, closeDb } from '../config/db.js';
+import { connectEditDb, closeDb } from "../config/db.js";
 
-const deleteGuestPolls = (req, res) => {
+const deleteGuestPolls = () => {
     //connect to the database
     let db = connectEditDb();
-    //delete all polls associated with the guest user that where created more than 48 hours ago
-    db.run('DELETE FROM polls WHERE user_id = 1 AND created_on < datetime("now", "-2 days")', (err) => {
+    //delete all polls associated with the guest user that were created more than 48 hours ago
+    db.run("DELETE FROM polls WHERE user_id IS NULL AND created_on < datetime('now', '-2 days')", (err) => {
         if (err) {
             console.error(err.message);
-            res.status(500).send('Internal server error');
         } else {
-            res.status(200).send('Polls deleted');
+            console.log("Polls deleted");
         }
+        //close the database
+        closeDb(db);
     });
-    //close the database
-    closeDb(db);
 };
 
 export default deleteGuestPolls;
