cb07e4ae1fb22daa6cd9e788ad0dd70bbf847ad5
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-09-23 15:12:21 +0100

    Refactor checkAuth middleware to use double quotes for consistency

--- backend/middleware/checkAuth.js
+++ backend/middleware/checkAuth.js
@@ -1,12 +1,12 @@
 // Load node modules
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 //Checks if the user is authenticated by verifying the token
 const checkAuth = async (req, res, next) => {
     try {
         const token = req.cookies.Authorization;
         if (!token) {
-            return res.status(401).json({ message: 'No Token' });
+            return res.status(401).json({ message: "No Token" });
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user_id = decoded.user_id;
